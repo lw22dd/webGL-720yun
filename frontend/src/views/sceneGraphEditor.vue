e2779199268be24053b381819e744c6bfb223a74
lw22dd <1322168192@qq.com>
2026-04-15 09:57:10 +0800

    feat：初步完成图编辑页面原型测试

---
+++ frontend/src/views/sceneGraphEditor.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="editor-layout">
+    <EditorToolbar />
+    <div class="editor-body">
+      <EditorCanvas />
+      <EditorSidebar />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import EditorToolbar from './EditorToolbar.vue'
+import EditorCanvas from './EditorCanvas.vue'
+import EditorSidebar from './EditorSidebar.vue'
+</script>
+
+<style scoped>
+.editor-layout {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+}
+
+.editor-body {
+  flex: 1;
+  display: flex;
+  overflow: hidden;
+  position: relative;
+}
+</style>
